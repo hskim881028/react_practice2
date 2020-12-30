1f2b98bb51740772cc679f6ac6e7535621dfc024
hskim <andykim881028@gmail.com>
2020-12-30 21:15:34 +0900

    practice input function component

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,56 +1,108 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class EventPractice extends Component {
-  state = {
-    userName: "",
+const EventPractice = () => {
+  const [form, setForm] = useState({
+    username: "",
     message: "",
-  };
+  });
 
-  handleChange = (e) => {
-    console.log(e.target.name + " : " + e.target.value);
-    this.setState({
+  const { username, message } = form;
+  const onChange = (e) => {
+    const nextForm = {
+      ...form,
       [e.target.name]: e.target.value,
-    });
+    };
+    setForm(nextForm);
   };
 
-  handleClick = (e) => {
-    alert(this.state.userName + " : " + this.state.message);
-    this.setState({
-      userName: "",
+  const onClick = () => {
+    alert(username + " : " + message);
+    setForm({
+      username: "",
       message: "",
     });
   };
 
-  handleKeyPress = (e) => {
-    if (e.key === "Enter") {
-      this.handleClick();
+  const onKeyPress = (e) => {
+    if (e.key === "enter") {
+      onClick();
     }
   };
 
-  render() {
-    return (
-      <div>
-        <h1>event practice</h1>
-        <input
-          type="text"
-          name="userName"
-          placeholder="사용자명"
-          value={this.state.userName}
-          onChange={this.handleChange}
-          onKeyPress={this.handleKeyPress}
-        />
-        <input
-          type="text"
-          name="message"
-          placeholder="암꺼나 입력해주세요!"
-          value={this.state.message}
-          onChange={this.handleChange}
-          onKeyPress={this.handleKeyPress}
-        />
-        <button onClick={this.handleClick}>confirm</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>키입력해주세요</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="메세지"
+        value={message}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>Click!</button>
+    </div>
+  );
+};
+
+// class EventPractice extends Component {
+//   state = {
+//     userName: "",
+//     message: "",
+//   };
+//
+//   handleChange = (e) => {
+//     console.log(e.target.name + " : " + e.target.value);
+//     this.setState({
+//       [e.target.name]: e.target.value,
+//     });
+//   };
+//
+//   handleClick = (e) => {
+//     alert(this.state.userName + " : " + this.state.message);
+//     this.setState({
+//       userName: "",
+//       message: "",
+//     });
+//   };
+//
+//   handleKeyPress = (e) => {
+//     if (e.key === "Enter") {
+//       this.handleClick();
+//     }
+//   };
+//
+//   render() {
+//     return (
+//       <div>
+//         <h1>event practice</h1>
+//         <input
+//           type="text"
+//           name="userName"
+//           placeholder="사용자명"
+//           value={this.state.userName}
+//           onChange={this.handleChange}
+//           onKeyPress={this.handleKeyPress}
+//         />
+//         <input
+//           type="text"
+//           name="message"
+//           placeholder="암꺼나 입력해주세요!"
+//           value={this.state.message}
+//           onChange={this.handleChange}
+//           onKeyPress={this.handleKeyPress}
+//         />
+//         <button onClick={this.handleClick}>confirm</button>
+//       </div>
+//     );
+//   }
+// }
 
 export default EventPractice;
