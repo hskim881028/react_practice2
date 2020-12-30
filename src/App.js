905dc789c756560e64bf9853614583de06c0e5f9
hskim <andykim881028@gmail.com>
2020-12-30 22:06:48 +0900

    Use ref in class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import EventPractice from "./EventPractice";
+//import EventPractice from "./EventPractice";
+//import ValidationSample from "./ValidationSample";
+import React, { Component } from "react";
+import ScrollBox from "./ScrollBox";
+//import { render } from "@testing-library/react";
 
-function App() {
-  return <EventPractice />;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
